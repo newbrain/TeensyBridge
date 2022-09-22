e03e9fc8b197a440035df385ad83e8416874d8a9
newbrain <newbrain@tiscali.it>
2022-09-23 00:43:59 +0200

    All features working.

---
+++ TeensyBridge.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2022, Federico Zuccardi Merli.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "bsp/board.h"
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "tusb.h"
+//#include <stdio.h>
+
+#define UART_TX 4
+#define UART_RX 5
+
+#define PROGRAM 2
+#define NMI     3
+
+#define DELAY 50 /* ms */
+
+int main(void)
+{
+    stdio_init_all();
+
+    /* Initialize PROGRAM as Open Drain like */
+    gpio_set_function(PROGRAM, GPIO_FUNC_SIO);        /* SIO for SW controlled GPIO */
+    gpio_set_dir(PROGRAM, GPIO_IN);                   /* Disable output */
+    gpio_set_pulls(PROGRAM, true, false);             /* Pull Up */
+    gpio_put(PROGRAM, false);                         /* Low when active */
+    gpio_set_slew_rate(PROGRAM, GPIO_SLEW_RATE_SLOW); /* No hurry */
+
+    /* Initialize PROGRAM as Open Source like */
+    gpio_set_function(NMI, GPIO_FUNC_SIO);        /* SIO for SW controlled GPIO */
+    gpio_set_dir(NMI, GPIO_IN);                   /* Disable output */
+    gpio_set_pulls(NMI, false, true);             /* Pull Down */
+    gpio_put(NMI, true);                          /* High when active */
+    gpio_set_slew_rate(NMI, GPIO_SLEW_RATE_SLOW); /* No hurry */
+
+    /* Initialize USB library */
+    board_init();
+    tusb_init();
+
+    uart_init(uart1, 115200);
+
+    gpio_set_function(UART_RX, GPIO_FUNC_UART);
+    gpio_set_function(UART_TX, GPIO_FUNC_UART);
+
+    alarm_pool_init_default();
+
+    for (;;)
+    {
+        tud_task();
+        uint32_t count = 0;
+        while (uart_is_writable(uart1) && tud_cdc_available())
+        {
+            char c = tud_cdc_read_char();
+            uart_putc_raw(uart1, c);
+        }
+        while (uart_is_readable(uart1) && tud_cdc_write_available())
+        {
+            char c = uart_getc(uart1);
+            tud_cdc_write_char(c);
+        }
+        tud_cdc_write_flush();
+    }
+    return 0;
+}
+
+int64_t pin_callback(alarm_id_t id, void *pin_vp)
+{
+    (void)id;
+    uintptr_t pin = (uintptr_t)pin_vp;
+    gpio_set_dir(pin, GPIO_IN);
+    board_led_off();
+    return 0;
+}
+
+void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const *line_options)
+{
+    (void)itf;
+    uint32_t pin;
+    if (line_options->bit_rate == 301)
+    {
+        gpio_set_dir(PROGRAM, GPIO_OUT);
+        board_led_on();
+        add_alarm_in_ms(DELAY, pin_callback, (void *)PROGRAM, false);
+    }
+    else if (line_options->bit_rate == 302)
+    {
+        gpio_set_dir(NMI, GPIO_OUT);
+        board_led_on();
+        add_alarm_in_ms(DELAY, pin_callback, (void *)NMI, false);
+    }
+    else
+    {
+        uart_set_baudrate(uart1, line_options->bit_rate);
+    }
+    /* Set the other line parameters NO ERROR CHECKING! */
+    uart_parity_t p = line_options->parity;
+    if (p == 2) p = 1;
+    uart_set_format(uart1,
+                    line_options->data_bits,
+                    line_options->stop_bits,
+                    line_options->parity);
+}
---
+++ tusb_config.h
@@ -0,0 +1,108 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ha Thach (tinyusb.org)
+ * Copyright (c) 2020 Jerzy Kasenberg
+ * Copyright (c) 2022 Federico Zuccardi Merli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//--------------------------------------------------------------------
+// COMMON CONFIGURATION
+//--------------------------------------------------------------------
+
+//#include "usb_descriptors.h"
+
+// defined by compiler flags for flexibility
+#ifndef CFG_TUSB_MCU
+#error CFG_TUSB_MCU must be defined
+#endif
+
+#if CFG_TUSB_MCU == OPT_MCU_LPC43XX || CFG_TUSB_MCU == OPT_MCU_LPC18XX || CFG_TUSB_MCU == OPT_MCU_MIMXRT10XX
+#define CFG_TUSB_RHPORT0_MODE       (OPT_MODE_DEVICE | OPT_MODE_HIGH_SPEED)
+#else
+#define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
+#endif
+
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS                 OPT_OS_NONE
+#endif
+
+#ifndef CFG_TUSB_DEBUG
+// Can be set during compilation i.e.: make LOG=<value for CFG_TUSB_DEBUG> BOARD=<bsp>
+// Keep in mind that enabling logs when data is streaming can disrupt data flow.
+// It can be very helpful though when audio unit requests are tested/debugged.
+#define CFG_TUSB_DEBUG              0
+#endif
+
+/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
+ * Tinyusb use follows macros to declare transferring memory so that they can be put
+ * into those specific section.
+ * e.g
+ * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
+ * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
+ */
+#ifndef CFG_TUSB_MEM_SECTION
+#define CFG_TUSB_MEM_SECTION
+#endif
+
+#ifndef CFG_TUSB_MEM_ALIGN
+#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
+#endif
+
+//--------------------------------------------------------------------
+// DEVICE CONFIGURATION
+//--------------------------------------------------------------------
+
+#ifndef CFG_TUD_ENDPOINT0_SIZE
+#define CFG_TUD_ENDPOINT0_SIZE    64
+#endif
+
+//------------- CLASS -------------//
+#define CFG_TUD_CDC               1
+#define CFG_TUD_MSC               0
+#define CFG_TUD_HID               0
+#define CFG_TUD_MIDI              0
+#define CFG_TUD_AUDIO             0
+#define CFG_TUD_VENDOR            0
+
+// CDC FIFO size of TX and RX
+#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+
+// CDC Endpoint transfer buffer size, more is faster
+#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+
+// MSC Buffer size of Device Mass storage
+#define CFG_TUD_MSC_EP_BUFSIZE   512
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif
