2299a0adc170f8a2a69e11bf60698f6d88533a48
newbrain <newbrain@tiscali.it>
2022-11-19 15:10:27 +0100

    Version with two ports

--- Readme.md
+++ Readme.md
@@ -2,17 +2,19 @@
 
 Serial, Program, and NMI interface for Teensy 4.x (or any other board!) on a Raspberry Pi Pico board.
 
+* USB to UART bridge on pins __GPIO0__ (Tx) and __GPIO1__ (Rx)<br>
+
 * USB to UART bridge on pins __GPIO4__ (Tx) and __GPIO5__ (Rx)<br>
 
 * Active LOW (pull up) Program signal for Teensy on __GPIO2__.<br>
-To simulate a 50 ms button press, change the USB serial port to __301__ baud (the actual UART speed won't change).
+To simulate a 50 ms button press, change the USB second serial port to __301__ baud (the actual UART speed won't change).
 
 * Active HIGH (pull down) NMI signal (or any other use) on __GPIO3__.<br>
-To simulate a 50 ms button press, change the USB serial port speed to __302__ baud (the actual UART speed won't change).
+To simulate a 50 ms button press, change the USB second serial port speed to __302__ baud (the actual UART speed won't change).
 
 * All other speed values and line parameters will actually change the UART settings.
 
-* The default line parameters are 115200 b/s, 8N1.
+* The default line parameters are 115200 b/s, 8N1 on the first serial port and 2 Mb/s, 8N1 on the second one.
 
 * Other pins can be easily associated with other speed settings.
 
--- TeensyBridge.c
+++ TeensyBridge.c
@@ -11,10 +11,13 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "tusb.h"
-//#include <stdio.h>
+#include "get_serial.h"
+// #include <stdio.h>
 
-#define UART_TX 4
-#define UART_RX 5
+#define UART0_TX 0
+#define UART0_RX 1
+#define UART1_TX 4
+#define UART1_RX 5
 
 #define PROGRAM 2
 #define NMI     3
@@ -23,8 +26,6 @@
 
 int main(void)
 {
-    stdio_init_all();
-
     /* Initialize PROGRAM as Open Drain like */
     gpio_set_function(PROGRAM, GPIO_FUNC_SIO);        /* SIO for SW controlled GPIO */
     gpio_set_dir(PROGRAM, GPIO_IN);                   /* Disable output */
@@ -41,35 +42,42 @@ int main(void)
 
     /* Initialize USB library */
     board_init();
+    usb_serial_init();
     tusb_init();
 
+    uart_init(uart0, 115200);
     uart_init(uart1, 115200);
 
-    gpio_set_function(UART_RX, GPIO_FUNC_UART);
-    gpio_set_function(UART_TX, GPIO_FUNC_UART);
+    gpio_set_function(UART0_RX, GPIO_FUNC_UART);
+    gpio_set_function(UART0_TX, GPIO_FUNC_UART);
+    gpio_set_function(UART1_RX, GPIO_FUNC_UART);
+    gpio_set_function(UART1_TX, GPIO_FUNC_UART);
 
     alarm_pool_init_default();
 
     for (;;)
     {
         tud_task();
-        uint32_t count = 0;
-        while (uart_is_writable(uart1) && tud_cdc_available())
+        for (uint8_t itf = 0; itf < 2; itf++)
         {
-            char c = tud_cdc_read_char();
-            uart_putc_raw(uart1, c);
+            uart_inst_t *uart = uart_get_instance(itf);
+            while (uart_is_writable(uart) && tud_cdc_n_available(itf))
+            {
+                char c = tud_cdc_n_read_char(itf);
+                uart_putc_raw(uart, c);
+            }
+            while (uart_is_readable(uart) && tud_cdc_n_write_available(itf))
+            {
+                char c = uart_getc(uart);
+                tud_cdc_n_write_char(itf, c);
+            }
+            tud_cdc_n_write_flush(itf);
         }
-        while (uart_is_readable(uart1) && tud_cdc_write_available())
-        {
-            char c = uart_getc(uart1);
-            tud_cdc_write_char(c);
-        }
-        tud_cdc_write_flush();
     }
     return 0;
 }
 
-int64_t pin_callback(alarm_id_t id, void *pin_vp)
+static int64_t key_release(alarm_id_t id, void *pin_vp)
 {
     (void)id;
     uintptr_t pin = (uintptr_t)pin_vp;
@@ -78,26 +86,31 @@ int64_t pin_callback(alarm_id_t id, void *pin_vp)
     return 0;
 }
 
+static void key_press(uint32_t key)
+{
+    gpio_set_dir(key, GPIO_OUT);
+    board_led_on();
+    add_alarm_in_ms(DELAY, key_release, (void *)key, false);
+}
+
 void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const *line_options)
 {
-    (void)itf;
     uint32_t pin;
-    if (line_options->bit_rate == 301)
+    if (itf == 1) /* Only do this for the debug interface */
     {
-        gpio_set_dir(PROGRAM, GPIO_OUT);
-        board_led_on();
-        add_alarm_in_ms(DELAY, pin_callback, (void *)PROGRAM, false);
+        if (line_options->bit_rate == 301)
+        {
+            key_press(PROGRAM);
+            return; /* Do nothing for the other options */
+        }
+        else if (line_options->bit_rate == 302)
+        {
+            key_press(NMI);
+            return; /* Do nothing for the other options */
+        }
     }
-    else if (line_options->bit_rate == 302)
-    {
-        gpio_set_dir(NMI, GPIO_OUT);
-        board_led_on();
-        add_alarm_in_ms(DELAY, pin_callback, (void *)NMI, false);
-    }
-    else
-    {
-        uart_set_baudrate(uart1, line_options->bit_rate);
-    }
+    uart_inst_t *uart = uart_get_instance(itf);
+    uart_set_baudrate(uart, line_options->bit_rate);
     /* Set the other line parameters NO ERROR CHECKING! */
     uart_parity_t p = line_options->parity;
     if (p > 2)
@@ -105,7 +118,7 @@ void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const *line_options)
     else if (p)
         p = 2 - p;
     uint32_t bits = line_options->data_bits == 16 ? 8 : line_options->data_bits;
-    uart_set_format(uart1,
+    uart_set_format(uart,
                     bits,
                     1 + (line_options->stop_bits >> 1),
                     p);
